2dac14824327e421d4c00968bb25c6c61ad70212
jeremy woo <jeremy.woo@akqa.com>
2018-09-06 13:00:10 +1200

    updated reactjs spec mark up.

--- src/components/topNavigation/topTiles.tsx
+++ src/components/topNavigation/topTiles.tsx
@@ -11,34 +11,34 @@ class TopTiles extends React.Component {
                     
             <div className="row tile_count">
             <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                <span className="count_top"><i className="fa fa-user"></i> Total Users</span>
+                <span className="count_top"><i className="fa fa-user"/> Total Users</span>
                 <div className="count">2500</div>
                 <span className="count_bottom"><i className="green">4% </i> From last Week</span>
             </div>
             <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                <span className="count_top"><i className="fa fa-clock-o"></i> Average Time</span>
+                <span className="count_top"><i className="fa fa-clock-o"/> Average Time</span>
                 <div className="count">123.50</div>
-                <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"></i>3% </i> From last Week</span>
+                <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"/>3% </i> From last Week</span>
             </div>
             <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                <span className="count_top"><i className="fa fa-user"></i> Total Males</span>
+                <span className="count_top"><i className="fa fa-user"/> Total Males</span>
                 <div className="count green">2,500</div>
-                <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"></i>34% </i> From last Week</span>
+                <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"/>34% </i> From last Week</span>
             </div>
             <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                <span className="count_top"><i className="fa fa-user"></i> Total Females</span>
+                <span className="count_top"><i className="fa fa-user"/> Total Females</span>
                 <div className="count">4,567</div>
-                <span className="count_bottom"><i className="red"><i className="fa fa-sort-desc"></i>12% </i> From last Week</span>
+                <span className="count_bottom"><i className="red"><i className="fa fa-sort-desc"/>12% </i> From last Week</span>
             </div>
             <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                <span className="count_top"><i className="fa fa-user"></i> Total Collections</span>
+                <span className="count_top"><i className="fa fa-user"/> Total Collections</span>
                 <div className="count">2,315</div>
-                <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"></i>34% </i> From last Week</span>
+                <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"/>34% </i> From last Week</span>
             </div>
             <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                <span className="count_top"><i className="fa fa-user"></i> Total Connections</span>
+                <span className="count_top"><i className="fa fa-user" /> Total Connections</span>
                 <div className="count">7,325</div>
-                <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"></i>34% </i> From last Week</span>
+                <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc"/>34% </i> From last Week</span>
             </div>
             </div>
 
